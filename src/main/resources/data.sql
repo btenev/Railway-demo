70083703dbaefc3b66b48a21fc011b9edb3879f3
btenev <boyan.tenev@gmail.com>
2024-04-12 16:52:22 +0300

    Add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO addresses(id, street_name, street_number, town)
+VALUES (1, 'Madara', '10', 'Haskovo');
+
+INSERT INTO users(id, age, name, occupation, address_id)
+VALUES (1, 20, 'Boyan', 'Programmer', 1);
